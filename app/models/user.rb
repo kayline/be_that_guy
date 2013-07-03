c67f70899f7111e6bbbaa4ea354f3ac7ace7d0df
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-07-03 14:30:19 -0700

    added bcrypt and has_secure_password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 	has_many :challenges_users
 	has_many :challenges, :through => :challenges_users
 
+	has_secure_password
+	validates_presence_of :password, :on => :create
+
 end
