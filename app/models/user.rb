94422e054109fee4263bb2cfde8f4ec45b2a8aa2
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-07-10 15:17:06 -0700

    created an available_challenges methos on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates_presence_of :password, :on => :create
 
+	def available_challenges
+		# eventually dependent on user point level
+		Challenge.where(:level => 0)
+	end
+
 end
