0ad47ce9ae48b6b09b278b1acae99bec1106ab31
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-07-10 12:06:38 -0700

    modified user creation to automatically create a session (login) if user save is succesful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ def create
 	p params
 	@user = User.new(user_params)
 	if @user.save
+		session[:user_id] = @user.id
 		redirect_to @user
 	else
 		redirect_to root_path, flash[:notice] = @user.errors.full_messages
