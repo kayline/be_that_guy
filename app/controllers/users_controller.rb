1bba334cfa674115d72082388c05185b84c156a9
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-07-10 11:34:48 -0700

    created users controller with create and show actions, basic users show view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+def create
+	p params
+	@user = User.new(user_params)
+	if @user.save
+		redirect_to @user
+	else
+		redirect_to root_path, flash[:notice] = @user.errors.full_messages
+	end
+end
+
+def show
+	@user = User.find(params[:id])
+end
+
+private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
